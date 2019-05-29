12862685b8bf7b4bdc3375333fa8cef003294dea
Vivek Kalyan <hello@vivekkalyan.com>
2019-05-30 00:26:32 +0800

    Add sampler class to use index on dataset

--- dataset.py
+++ dataset.py
@@ -4,7 +4,8 @@ from PIL import Image
 import numpy as np
 import pandas as pd
 
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, Sampler
+
 
 class StanfordCarsDataset(Dataset):
     def __init__(self, csv_file, root_dir, transform=None):
@@ -32,6 +33,31 @@ class StanfordCarsDataset(Dataset):
             item = self.transform(item)
 
         return item
+
+
+class IndexSampler(Sampler):
+    """
+    Args:
+        dataset (Dataset): A dataset to sample from
+        data_idx (array): Indexes that correspond to dataset class
+        shuffle (boolean): Shuffle or not
+    """
+    def __init__(self, dataset, data_idx, shuffle=True):
+        self.dataset = dataset
+        self.data_idx = data_idx
+        self.shuffle = shuffle
+
+    def __iter__(self):
+        if self.shuffle:
+            rand_idx = np.random.permutation(self.data_idx)
+            for i in rand_idx:
+                yield self.dataset[i]
+        else:
+            for i in self.data_idx:
+                yield self.dataset[i]
+
+    def __len__(self):
+        return len(self.data_idx)
 
 
 def split_by_rand_pct_idx(df, valid_pct=0.2):
