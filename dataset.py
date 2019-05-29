b434773d723ece513c5115e8002bce072b63eaf2
Vivek Kalyan <hello@vivekkalyan.com>
2019-05-30 00:26:12 +0800

    Add function to split dataset into indexes

--- dataset.py
+++ dataset.py
@@ -1,6 +1,7 @@
 import os
 
 from PIL import Image
+import numpy as np
 import pandas as pd
 
 from torch.utils.data import Dataset
@@ -31,3 +32,12 @@ class StanfordCarsDataset(Dataset):
             item = self.transform(item)
 
         return item
+
+
+def split_by_rand_pct_idx(df, valid_pct=0.2):
+    "Split dataset/df randomly by return train/valid indexes"
+    split = int(len(df) * valid_pct)
+    rand_idx = np.random.permutation(list(range(len(df))))
+    train = rand_idx[:-split]
+    test = rand_idx[-split:]
+    return train, test
