f730b06535b6e88f0769a9b9a3218629a608d25a
Vivek Kalyan <hello@vivekkalyan.com>
2019-06-05 23:19:25 +0800

    Add concat pooling and 2 linear layers

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
-import torchvision
+import torch
 import torch.nn as nn
+import torchvision
 
 from utils import device
 
@@ -8,14 +9,19 @@ class Resnet34(nn.Module):
         super().__init__()
         model = torchvision.models.resnet34(pretrained=True)
         in_features = model.fc.in_features
-        self.resnet = nn.Sequential(*(list(model.children())[:-1]))
-        self.fc = LinearLayer(in_features, out_features)
+        self.resnet = nn.Sequential(*(list(model.children())[:-2]))
+        self.concat_pool = AdaptiveConcatPool2d()
+        self.fc1 = LinearLayer(in_features*2, 512, dropout=0.25,
+                activation=nn.ReLU())
+        self.fc2 = LinearLayer(512, out_features, dropout=0.5)
 
     def forward(self, inputs):
         x = inputs['image'].to(device())
         x = self.resnet(x)
-        x = x.squeeze(dim=3).squeeze(dim=2)
-        x = self.fc(x)
+        x = self.concat_pool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc1(x)
+        x = self.fc2(x)
         return x
 
     def freeze(self):
@@ -49,3 +55,14 @@ class BatchNorm1dFlat(nn.BatchNorm1d):
         x = x.contiguous().view(-1, c)
         return super().forward(x).view(*f, c)
 
+class AdaptiveConcatPool2d(nn.Module):
+    "Layer that concats `AdaptiveAvgPool2d` and `AdaptiveMaxPool2d`."
+    def __init__(self, output_size=None):
+        "Output will be 2*sz or 2 if sz is None"
+        super().__init__()
+        self.output_size = output_size or 1
+        self.ap = nn.AdaptiveAvgPool2d(self.output_size)
+        self.mp = nn.AdaptiveMaxPool2d(self.output_size)
+
+    def forward(self, x):
+        return torch.cat([self.mp(x), self.ap(x)], 1)
